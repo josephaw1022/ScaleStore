6450bcd3af031bcd112d15f0b490d4f12e345822
Joe Whiteaker <47674962+josephaw1022@users.noreply.github.com>
2023-12-26 16:24:25 -0500

    remove unnecassary import

--- ServiceScalingDb/ScalingDb/ScalingDbInitializer.cs
+++ ServiceScalingDb/ScalingDb/ScalingDbInitializer.cs
@@ -3,7 +3,6 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using System.Diagnostics;
-using Microsoft.VisualBasic;
 
 
 namespace ServiceScalingDb.ScalingDb;

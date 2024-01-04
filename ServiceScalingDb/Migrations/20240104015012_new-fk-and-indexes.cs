fd1e2a1a482ac5300d73855c04d656a98a012126
Joe Whiteaker <47674962+josephaw1022@users.noreply.github.com>
2024-01-03 20:53:17 -0500

    new migrations for indexes and keys in db

---
+++ ServiceScalingDb/Migrations/20240104015012_new-fk-and-indexes.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ServiceScalingDb.Migrations
+{
+    /// <inheritdoc />
+    public partial class newfkandindexes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ScalingConfigurations_Applications_ApplicationID",
+                table: "ScalingConfigurations");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ScalingConfigurations_Environments_EnvironmentID",
+                table: "ScalingConfigurations");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ScalingConfigurations_Applications_ApplicationID",
+                table: "ScalingConfigurations",
+                column: "ApplicationID",
+                principalTable: "Applications",
+                principalColumn: "ApplicationID",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ScalingConfigurations_Environments_EnvironmentID",
+                table: "ScalingConfigurations",
+                column: "EnvironmentID",
+                principalTable: "Environments",
+                principalColumn: "EnvironmentID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ScalingConfigurations_Applications_ApplicationID",
+                table: "ScalingConfigurations");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ScalingConfigurations_Environments_EnvironmentID",
+                table: "ScalingConfigurations");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ScalingConfigurations_Applications_ApplicationID",
+                table: "ScalingConfigurations",
+                column: "ApplicationID",
+                principalTable: "Applications",
+                principalColumn: "ApplicationID",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ScalingConfigurations_Environments_EnvironmentID",
+                table: "ScalingConfigurations",
+                column: "EnvironmentID",
+                principalTable: "Environments",
+                principalColumn: "EnvironmentID",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}

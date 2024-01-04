cbca7d02520ad6b71467205d03fe3930458b034f
Joe Whiteaker <47674962+josephaw1022@users.noreply.github.com>
2024-01-03 21:22:26 -0500

    add new instance count function

---
+++ ServiceScalingDb/Migrations/20240104015416_instance-count-function.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+namespace ServiceScalingDb.Migrations
+{
+    /// <inheritdoc />
+    public partial class instancecountfunction : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                CREATE OR REPLACE FUNCTION get_number_of_instances(
+                    applicationName VARCHAR,
+                    projectName VARCHAR,
+                    environmentName VARCHAR
+                )
+                RETURNS SETOF integer AS $$
+                BEGIN
+                    RETURN QUERY
+                    SELECT SC.""NumberOfInstances""
+                    FROM public.""ScalingConfigurations"" AS SC
+                    INNER JOIN public.""Applications"" AS A ON SC.""ApplicationID"" = A.""ApplicationID""
+                    INNER JOIN public.""Projects"" AS P ON A.""ProjectID"" = P.""ProjectID""
+                    INNER JOIN public.""Environments"" AS E ON SC.""EnvironmentID"" = E.""EnvironmentID""
+                    WHERE A.""ApplicationName"" = applicationName
+                    AND P.""ProjectName"" = projectName
+                    AND E.""EnvironmentName"" = environmentName;
+                END;
+                $$ LANGUAGE plpgsql;
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                DROP FUNCTION IF EXISTS get_number_of_instances(VARCHAR, VARCHAR, VARCHAR);
+            ");
+        }
+    }
+}

174f23e86e58f7727bb1cd20151109c0cd829f0a
Joe Whiteaker <47674962+josephaw1022@users.noreply.github.com>
2024-01-20 03:30:25 -0500

    Get to a decent stopping point for e2e poc for future iterations

---
+++ E2EWebUI/Pages/BaseScaleStoreWebUIPage.cs
@@ -0,0 +1,9 @@
+﻿using System.IO;
+
+namespace E2EWebUI.Pages
+{
+    internal static class ScaleStoreWebUIPage
+    {
+        public static string BaseUrl { get;  } = "http://localhost:5049";
+    }
+}
